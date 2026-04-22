0b1431fa5cf0065061ac4099bd4ce9fc68321de6
Clemens Watzenböck <clemens.watzenboeck@meduniwien.ac.at>
2026-04-22 15:29:08 +0200

    Register buffer

--- gradient_reversal/module.py
+++ gradient_reversal/module.py
@@ -1,11 +1,27 @@
-from .functional import revgrad
+from .functional import revgrad, scalegrad
 import torch
 from torch import nn
 
 class GradientReversal(nn.Module):
     def __init__(self, alpha):
         super().__init__()
-        self.alpha = torch.tensor(alpha, requires_grad=False)
+        self.register_buffer("alpha", torch.tensor(float(alpha), dtype=torch.float32, requires_grad=False))
 
     def forward(self, x):
         return revgrad(x, self.alpha)
+
+    def set_alpha(self, alpha: float):
+        self.alpha.fill_(float(alpha))
+
+# class GradientScaler(nn.Module):
+#     def __init__(self, alpha):
+#         super().__init__()
+#         self.alpha = torch.tensor(alpha, requires_grad=False)
+
+#     def forward(self, x):
+#         return (x, self.alpha)
+
+## Not really neccesary ... 
+#  Just use:  
+##   y = x * alpha + x.detach() * (1 - alpha)
+#
